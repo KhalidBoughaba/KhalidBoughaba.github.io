638b7f1b1d9ad604907cd93d07af7ec1acc3deb8
Khalid Boughaba <boughaba.khalid.dev@gmail.com>
2022-05-06 11:03:24 +0000

    add portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+  strings: ["", "Web Developer", "Freelancer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+console.log()
+
